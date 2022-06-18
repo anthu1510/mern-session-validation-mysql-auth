966a9d070fc73883bfa2baf3621b21ec5cf34979
anthu1510 <anthu1510@gmail.com>
2022-06-18 19:16:05 +0530

    client integration done

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-12">
+          <h1>Welcome</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
